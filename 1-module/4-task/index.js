71a800de90cf97442a81aba20193d43fb16d2a2a
Anton Chuvirau <antonchuvirov@icloud.com>
2020-04-02 11:35:23 +0300

    Added changes

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,13 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    let strToLowerCase = str.toLowerCase();
+    let firstSubStrToLowerCase = '1xBet'.toLowerCase();
+    let secondSubStrToLowerCase = 'XXX'.toLowerCase();
+    if (strToLowerCase.includes(firstSubStrToLowerCase) || strToLowerCase.includes(secondSubStrToLowerCase)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
