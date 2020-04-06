f2a1d773b776771b568c5affd1c0799dc189c773
Anton Chuvirau <antonchuvirov@icloud.com>
2020-04-06 11:21:27 +0300

    Added changes

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+    let salariesSum = 0;
+    for (let prop in salaries){
+        if (typeof salaries[prop] === 'number'){
+            salariesSum+=salaries[prop];
+        }
+        else{
+            continue;
+        }
+    }
+    return salariesSum;
 }
