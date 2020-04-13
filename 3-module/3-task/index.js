2e24735eedbcd4f94b929100e7a288dfa5192f9d
Anton Chuvirau <antonchuvirov@icloud.com>
2020-04-13 11:06:08 +0300

    Added changes

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,16 @@
  * @returns {string}
  */
 function camelize(str) {
+    if (str){
+        let strToArray = str.split('');
+        strToArray.forEach((item, index)=>{
+            if (item === '-'){
+                strToArray[index+1] = strToArray[index+1].toUpperCase();
+            }
+        });
+        return strToArray.join('').replace(/-/g, '');
+    }
+    else {
+        return str;
+    }
 }
