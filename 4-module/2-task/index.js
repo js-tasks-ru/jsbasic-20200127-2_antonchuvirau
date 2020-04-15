48a98ff7e38d977582d1495d547100ae14f0231a
Anton Chuvirau <antonchuvirov@icloud.com>
2020-04-15 14:36:03 +0300

    Merge branch 'master' of https://github.com/js-tasks-ru/jsbasic-20200127-2_antonchuvirau

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,11 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    let tableRows = table.rows;
+    let defaultCellIndex = 0;
+    for (let tableRow of tableRows){
+        let tableRowCells = tableRow.cells;
+        tableRowCells[defaultCellIndex].style.backgroundColor = 'red';
+        defaultCellIndex++;
+    }
 }
