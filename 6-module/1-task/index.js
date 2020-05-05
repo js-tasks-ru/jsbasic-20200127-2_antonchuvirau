1c2988fa2f76f3d9ba5738e159e80b24bbb7d81d
Anton Chuvirau <antonchuvirov@icloud.com>
2020-05-05 12:43:05 +0300

    Added changes

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -17,13 +17,67 @@ class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
     this.data = data;
+    this.render(this.data);
+    this.el.addEventListener('click', (event) => this.onClicked(event));
   }
-
+  render(data) {
+    this.el.createTHead();
+    this.el.createTBody();
+    this.el.querySelector('thead').append(document.createElement('tr'));
+    let tableTitles = Object.keys(data[0]);
+    //Fill table header
+    for (let key of tableTitles) {
+      let tableHeaderCell = document.createElement('td');
+      tableHeaderCell.textContent = key;
+      this.el.querySelector('thead tr').append(tableHeaderCell);
+    }
+    //Fill table body
+    for (let item of data) {
+      let tableBodyRow = document.createElement('tr');
+      let itemValues = Object.values(item);
+      for (let value of itemValues) {
+        let tableBodyCell = document.createElement('td');
+        tableBodyCell.textContent = value;
+        tableBodyRow.append(tableBodyCell);
+      }
+      this.el.querySelector('tbody').append(tableBodyRow);
+    }
+  }
+  render(data) {
+    this.el.createTHead();
+    this.el.createTBody();
+    let tableHeader = this.el.querySelector('thead').innerHTML = '<tr></tr>';
+    let tableTitles = Object.keys(data[0]);
+    //Fill table header
+    for (let key of tableTitles) {
+      let tableHeaderCell = document.createElement('td');
+      tableHeaderCell.textContent = key;
+      this.el.querySelector('thead tr').append(tableHeaderCell);
+    }
+    this.el.querySelector('thead tr').append(document.createElement('td'));
+    //Fill table body
+    for (let item of data) {
+      let tableBodyRow = document.createElement('tr');
+      let itemValues = Object.values(item);
+      const list = itemValues.map(value => `<td>${value}</td>`).join('');
+      tableBodyRow.innerHTML = list + `<td><a>X</a></td>`;
+      this.el.querySelector('tbody').append(tableBodyRow);
+    }
+  }
+  onClicked(event) {
+    if (event.target.closest('a')) {
+      let id = event.target.closest('tr').firstElementChild.textContent;
+      event.target.closest('tr').remove();
+      this.onRemoved(+id);
+    }
+  }
   /**
    * Метод который вызывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
-  onRemoved(id) {}
+  onRemoved(id) {
+    console.log(`Из таблицы удален пользователь ${id}`);
+  }
 }
 
 window.ClearedTable = ClearedTable;
